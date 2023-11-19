cc63515ddc33cbad14075252b86a2a01d87296fa
Michał Jaworski <252329@edu.p.lodz.pl>
2023-11-19 13:32:44 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Git.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Git.dir/main.cpp.o"
+  "Git"
+  "Git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
